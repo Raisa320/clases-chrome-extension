85876895e09579da5f1fda4548f0b28b6d0ec715
Raisa320 <67403486+Raisa320@users.noreply.github.com>
2023-03-30 18:08:40 -0500

    modificaciones

--- contentscript.js
+++ contentscript.js
@@ -1,15 +1,72 @@
 console.log("Ejecutando el content script 1.0");
 
-chrome.runtime.onConnect.addListener(function (port) {
-  port.onMessage.addListener(function ({ message }) {
-    alert(`${port.name}: message`);
-    if (message === "hola") port.postMessage({ message: "Como estas?" });
+function verifyBenefits(benefits) {
+  if (benefits.className.includes("fresnel-container")) {
+    return "No se detalla";
+  }
+  return (benefits = benefits.innerText.split("\n"));
+}
+
+function getJobsInformation() {
+  const elementCardJobs = [...document.querySelectorAll("[id^='jobcard-']")];
+  const jobs = elementCardJobs.map((cardJob) => {
+    let [
+      { href: url },
+      {
+        children: [
+          {
+            children: [
+              { innerText: date },
+              { innerText: title },
+              { innerText: salario },
+              benefits,
+              infoCompany,
+            ],
+          },
+        ],
+      },
+    ] = cardJob.children;
+
+    if (infoCompany.className.includes("fresnel-container")) {
+      infoCompany = infoCompany.nextSibling;
+    }
+
+    [infoCompany] = infoCompany.children;
+    const companyName = infoCompany?.querySelectorAll("label")[0].innerText;
+    const companyCity = infoCompany?.querySelectorAll("p")[0].innerText;
+
+    return {
+      url,
+      date: date.split("\n")[0],
+      title,
+      salario,
+      benefits: verifyBenefits(benefits),
+      companyName,
+      companyCity,
+    };
   });
-});
+  return jobs;
+}
 
 //Connect to background
-const port = chrome.runtime.connect({ name: "content-background" });
-port.postMessage({ message: "Hola Background" });
-port.onMessage.addListener(async ({ message }) => {
-  alert(message);
+const portBackground = chrome.runtime.connect({ name: "content-background" });
+
+portBackground.onMessage.addListener(async ({ message }) => {
+  if (message === "nextpage") {
+    const nextPageBtn = document.querySelector("[class*='next-']");
+    nextPageBtn.click();
+    //portBackground.postMessage({ message: "startscrap" });
+  }
+});
+
+chrome.runtime.onConnect.addListener(function (port) {
+  port.onMessage.addListener(function ({ message }) {
+    console.log(`${port.name}: message`);
+    if (message === "getJobs") {
+      const jobs = getJobsInformation();
+      port.postMessage({ message: "ok", data: jobs });
+
+      portBackground.postMessage({ message: "finish" });
+    }
+  });
 });
--- popup/index.js
+++ popup/index.js
@@ -1,18 +1,20 @@
 const btnScripting = document.getElementById("btncomunicacion");
 const btnScriptingBackground = document.getElementById("btncomunicacionbckg");
+const pMensaje = document.getElementById("mensajes");
 
 btnScripting.addEventListener("click", async () => {
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   let port = chrome.tabs.connect(tab.id, { name: "popup" });
-  port.postMessage({ message: "hola" });
-  port.onMessage.addListener(function ({ message }) {
-    alert(message);
+  port.postMessage({ message: "getJobs" });
+  port.onMessage.addListener(({ message,data }) => {
+    if(message==="ok")
+      pMensaje.innerText = JSON.stringify(data, null, 2);
   });
 });
 
 btnScriptingBackground.addEventListener("click", async () => {
-  var port = chrome.runtime.connect({ name: "popup-background" });
-  port.postMessage({ message: "Hola BD" });
+  let port = chrome.runtime.connect({ name: "popup-background" });
+  port.postMessage({ message: "startscrap" });
   port.onMessage.addListener(function ({ message }) {
     alert(message);
   });
